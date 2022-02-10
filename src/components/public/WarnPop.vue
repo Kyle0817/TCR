075d8b0e58444a7c1950d5359affd0407f6702ad
Jing-siao <vicky820715@gmail.com>
2022-02-10 10:33:15 +0800

    修改css增加component

---
+++ src/components/public/WarnPop.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="warnPop">
+    <div class="overlay"></div>
+    <div class="popBox outBox warnBox">
+      <p v-html="warnText" class="twoLines"></p>
+      <button class="warnBtn" @click="closePop(false)">確認</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['warnPop', 'warnText'],
+  emits: ['closePop'],
+  data() {
+    return {};
+  },
+  methods: {
+    closePop(res) {
+      this.$emit('closePop', res);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import '@/assets/public/scss/_pop.scss';
+</style>
---
+++ src/data/capacity.js
@@ -0,0 +1,23 @@
+export default {
+  // 幣桶容量
+  capacityCRF: [
+    {
+      Denomination: 50,
+      Value: 180,
+    },
+    {
+      Denomination: 10,
+      Value: 220,
+    },
+    {
+      Denomination: 5,
+      Value: 400,
+    },
+    {
+      Denomination: 1,
+      Value: 380,
+    },
+  ],
+  // 鈔箱容量
+  capacityTCR: 2750,
+};
